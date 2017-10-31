a0d9281216d4930185b6dd59feca4a7cda571cd7
phurinsawangphul <31012390+phurinsawangphul@users.noreply.github.com>
2017-10-31 12:44:49 +0700

    Update urls.py

--- Class_Management/urls.py
+++ Class_Management/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 app_name = 'Class_Management'
 urlpatterns = [
-    url(r'^$', views.index, name='CLASSROOM'),
-    url(r'^(?P<className>[0-9]+)/$', views.inside, name='Inside'),
+    url(r'^$', views.Home, name='CLASSROOM'),
     url(r'^Assignment/', include('Assign_Management.urls')),
     url(r'^Home/', views.Home, name='Home'),
     url(r'^Submit/$', views.Submit, name='SubmitRoom')
