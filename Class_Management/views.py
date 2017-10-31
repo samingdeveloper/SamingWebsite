0d37a5be9d7826f60f781c68f7ff5f654f264f98
phurinsawangphul <31012390+phurinsawangphul@users.noreply.github.com>
2017-10-31 12:45:18 +0700

    Update views.py

--- Class_Management/views.py
+++ Class_Management/views.py
@@ -3,20 +3,6 @@ from .models import ClassRoom,Quiz
 from django.http import Http404
 from django.contrib.auth.models import User
 
-def index(request):
-    list_classroom = ClassRoom.objects.all()
-    context = {
-        'list_classroom': list_classroom
-    }
-    return render(request, 'Classroom.html', context)
-
-def inside(request,className):
-    try:
-        quiz = ClassRoom.objects.get(pk=className)
-    except ClassRoom.DoesNotExist:
-        raise Http404("Classroom does not exist")
-    return render(request, 'Inside.html', {'quiz': quiz})
-
 def Home(request):
     var = request.session['var']
     if User.objects.get(username=var).extraauth.year:
